02998d1b6b720b9b84fe10379eef84eef9571880
ChernyHleb <popovarlol@gmail.com>
2023-04-06 23:32:00 +0300

    Implemented error handling with appropriate HTTP codes

--- auth_server.js
+++ auth_server.js
@@ -18,55 +18,105 @@ app.post("/login", (req, res) => {
     const accessToken = generateAccessToken(user);
     const refreshToken = jwt.sign(user, process.env.REFRESH_TOKEN_SECRET);
 
-    User.findOne(
-        { where: { email: email } }
-    ).then((resUser) => { 
-        if(resUser != null && resUser.pwd == password) {
-            User.update(
-                { token: refreshToken },
-                { where: { email: email }}
-            ).then(() => {
-                console.log("User " + email + ": token updated");
+    if(validateEmail(email)){
+        User.findOne(
+            { where: { email: email } }
+        ).then((resUser) => { 
+            if(resUser != null){
+                if(resUser.pwd == password) {
+                    User.update(
+                        { token: refreshToken },
+                        { where: { email: email }}
+                    ).then(() => {
+                        console.log("User " + email + ": token updated");
+                        res.status(201);
+                        res.json({ accessToken: accessToken, refreshToken: refreshToken });
+                    }).catch((err)=>{
+                        console.log(err);
+                        res.status(500);
+                        res.json({
+                            message: "Internal Server Error",
+                            error: err.message
+                        });
+                    });
+                }
+                else {
+                    res.status(403);
+                    res.json({
+                        message: "Forbidden",
+                        error: `Incorrect password for user ${email}`
+                    });
+                }
+            }
+            else{
+                res.status(400);
+                res.json({
+                    message: "Bad Request",
+                    error: `User ${email} doesnt exist`
+                });
+            }
+        }).catch((err) => {
+            console.log(err);
+            res.status(500);
+            res.json({
+                message: "Internal Server Error",
+                error: err.message
             });
-
-            res.json({ accessToken: accessToken, refreshToken: refreshToken });
-        }
-        else {
-            return res.sendStatus(403);
-        }
-    }).catch((err) => {
-        console.log(err);
-    });
+        });
+    }
+    else{
+        res.status(400);
+        res.json({
+            message: "Bad Request",
+            error: "email format is incorrect"
+        });
+    }
 });
 
 app.post("/token", (req, res) => {
     const refreshToken = req.body.token;
 
-    if (refreshToken == null) {
-        return res.sendStatus(401);
-    }
-
-    User.findOne({ where: { token: refreshToken } })
-    .then((user) => {
-        if(user == null){
-            return res.sendStatus(403);
-        }
-
-        jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
-            if(err){
-                res.sendStatus(403);
-            }
-            const accessToken = generateAccessToken({ name: user.name });
-            res.json({ accessToken: accessToken });
+    if(refreshToken == "" || refreshToken == null) {
+        res.status(401);
+        res.json({
+            message: "Unauthorized",
+            error: "refreshToken is null"
         });
-    })
-    .catch((err) => {
-        console.log(err);
-    });
+    }
+    else{
+        User.findOne({ where: { token: refreshToken } })
+        .then((user) => {
+            if(user == null){
+                return res.sendStatus(403);
+            }
+
+            jwt.verify(refreshToken, process.env.REFRESH_TOKEN_SECRET, (err, user) => {
+                if(err){
+                    return res.sendStatus(403);
+                }
+                const accessToken = generateAccessToken({ name: user.name });
+                res.status(201);
+                res.json({ accessToken: accessToken });
+            });
+        })
+        .catch((err) => {
+            console.log(err);
+            res.status(500);
+            res.json({
+                message: "Internal Server Error",
+                error: err.message
+            });
+        });
+    }
 });
 
 function generateAccessToken(user) {
     return jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '15s' });
+}
+
+function validateEmail(email) {
+    var re = /\S+@\S+\.\S+/;
+    return re.test(email);
 }
 
 app.delete('/logout', (req, res) => {
@@ -77,11 +127,31 @@ app.delete('/logout', (req, res) => {
         { where: { token: refreshToken }}
     ).then(() => {
         res.sendStatus(204);
-    })
-    .catch((err) => {
+    }).catch((err) => {
         console.log(err);
+        res.status(500);
+        res.json({
+            message: "Internal Server Error",
+            error: err.message
+        });
     });
 });
+
+// error 400 handler
+app.use((req, res, next) => {
+    const error = Error('400 Bad Request');
+    error.status = 400;
+    next(error);
+});
+
+// other errors handler
+app.use((err, req, res, next) => {
+    res.status(err.status || 500);
+    res.json({
+        message: String(err.status),
+        error: err.message
+    });
+}); 
 
 // DB Sync and running server
 db.sequelize.sync().then(() => {
